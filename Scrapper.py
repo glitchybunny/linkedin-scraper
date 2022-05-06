4ca6fbc7dedb11cfdfe8115e839ae33712278b56
Glitch <12066089+glitchybunny@users.noreply.github.com>
2022-05-06 12:42:20 +1000

    Fixed a few errors, added regular saving

--- Scrapper.py
+++ Scrapper.py
@@ -22,46 +22,52 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 import os.path
 from os import getenv
 
 
 def extract_section(section_id):
     section = []
-    section_items = driver.find_element(By.ID, section_id) \
-        .find_elements(By.XPATH,
-                       '..//ul//li[@class="artdeco-list__item pvs-list__item--line-separated pvs-list__item--one-column"]')
-    if VERBOSE: print(len(section_items), section_id)
+    try:
+        section_items = driver.find_element(By.ID, section_id) \
+            .find_elements(By.XPATH,
+                           '..//ul//li[@class="artdeco-list__item pvs-list__item--line-separated pvs-list__item--one-column"]')
+        if VERBOSE: print(len(section_items), section_id)
 
-    for item in section_items:
-        actions.move_to_element(item).perform()
+        for item in section_items:
+            actions.move_to_element(item).perform()
 
-        # Get item title
-        title = item.find_element(By.CLASS_NAME, 'mr1') \
-            .find_element(By.CLASS_NAME, "visually-hidden").text.strip()
+            # Get item title
+            title = item.find_element(By.CLASS_NAME, 'mr1') \
+                .find_element(By.CLASS_NAME, "visually-hidden").text.strip()
 
-        # Get item subtitle
-        try:
-            subtitle = item.find_element(By.CLASS_NAME, 't-14') \
-                .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
-        except:
-            subtitle = None
+            # Get item subtitle
+            try:
+                subtitle = item.find_element(By.CLASS_NAME, 't-14') \
+                    .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
+            except:
+                subtitle = None
 
-        # Get item dates, location
-        try:
-            dates = item.find_element(By.CLASS_NAME, 't-black--light') \
-                .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
-        except:
-            dates = None
+            # Get item dates, location
+            try:
+                dates = item.find_element(By.CLASS_NAME, 't-black--light') \
+                    .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
+            except:
+                dates = None
 
-        # Get item description
-        try:
-            desc = item.find_element(By.CLASS_NAME, 'pvs-list') \
-                .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
-        except:
-            desc = None
+            # Get item description
+            try:
+                desc = item.find_element(By.CLASS_NAME, 'pvs-list') \
+                    .find_element(By.CLASS_NAME, 'visually-hidden').text.strip()
+            except:
+                desc = None
 
-        section.append({"Title": title, "Subtitle": subtitle, "Dates": dates, "Description": desc})
+            section.append({"Title": title, "Subtitle": subtitle, "Dates": dates, "Description": desc})
+
+    except NoSuchElementException:
+        # Section doesn't exist on page, skip
+        if VERBOSE: print("0", section_id)
 
     return section
 
@@ -72,6 +78,11 @@ if __name__ == '__main__':
     PASSWORD = getenv('PASSWORD')
     VERBOSE = True
     SCRAPED_DATA = {}
+
+    # Load previously scraped data (if any)
+    if os.path.exists("output.json"):
+        with open("output.json", "r") as file:
+            SCRAPED_DATA = json.load(file)
 
     # Load URLs to download
     with open("urls.txt") as file:  # file with each URL on a new line
@@ -97,7 +108,7 @@ if __name__ == '__main__':
 
     # Go to each page and download publicly available user information
     for url in urls:
-        if len(url) > 0:
+        if len(url) > 0 and url not in SCRAPED_DATA:
             # Load page
             driver.get(url)
             sleep(3)
@@ -112,17 +123,16 @@ if __name__ == '__main__':
             for button in buttons:
                 if "see" in button.text and "more" in button.text:
                     actions.move_to_element(button).click(button).perform()
-                    sleep(0.5)
+                    sleep(0.2)
 
             # Scrape basic info from page
             sel = Selector(text=driver.page_source)
             name = sel.xpath(
                 '//*[@class = "text-heading-xlarge inline t-24 v-align-middle break-words"]/text()').extract_first().strip()
+            print(name)
             title = sel.xpath('//*[@class = "text-body-medium break-words"]/text()').extract_first().strip()
             about = sel.xpath(
                 '//*[@class="pv-shared-text-with-see-more t-14 t-normal t-black display-flex align-items-center"]/div/span[@class="visually-hidden"]/text()').extract_first().strip()
-
-            print("Scraping:", name)
 
             # Scrape sections
             experience = extract_section("experience")
@@ -138,9 +148,11 @@ if __name__ == '__main__':
                 "Volunteering": volunteering,
                 "Education": education
             }
-            sleep(10)
 
-    with open('output.json', 'w') as f:
-        json.dump(SCRAPED_DATA, f)
+            # Save after every single scrape, since linkedin likes logging out the scraper
+            with open('output.json', 'w') as file:
+                json.dump(SCRAPED_DATA, file)
+
+            sleep(1)
 
     driver.quit()
